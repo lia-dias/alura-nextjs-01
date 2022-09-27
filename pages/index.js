4186afe0a7d96c3e86b70fd6246b6b3dd720d8a3
Lia Dias <liadias914@gmail.com>
2022-09-27 19:44:03 -0300

    Criação da página inicial e FAQ

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function HomePage() {
+    return (
+        <div>
+            <h1>Alura Cases - Home Page</h1>
+            <a href="/faq">Confira nosso FAQ</a>        
+        </div>
+    )
+}
